8600dd42c0b4433c727b5817266c4ec1951dae23
huyongxiang@zdsoft.cn <huyongxiang@zdsoft.cn>
2020-09-23 15:03:30 +0800

    fix

---
+++ express-dome/mongoess.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+var kittySchema = mongoose.Schema({
+  name: String,
+});
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var felyne = new Kitten({ name: 'Felyne' });
+
+felyne.save(function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('success');
+  }
+});
--- mongodb/README.md
+++ mongodb/README.md
@@ -13,4 +13,19 @@
 
 - 控制台输入 exit
 
-+ show dbs 查看数据库列表
+* show dbs 查看数据库列表
+
+* db 查看当前的数据库
+
+* use 数据库名称 （没有就会创建）
+
++ db.集合名.insert() 添加数据 
+
+- show collections 查看当前集合
+
+- db.集合名.find() 查看集合内容
+
+### 在node 中如何操作MongoDB
+
++ 1. 使用官方的MongoDB包 node-mongodb-native
++ 2. 使用三方包 mongose http://www.mongoosejs.net/docs/index.html
