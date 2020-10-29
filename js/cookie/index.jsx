4ca6b7ded3070f77eec18832dbe61806e1848b78
huyongxiang@zdsoft.cn <huyongxiang@zdsoft.cn>
2020-10-29 11:14:29 +0800

    fix

--- js/cookie/index.jsx
+++ js/cookie/index.jsx
@@ -3,3 +3,18 @@ cookie
 localStorage
 
 sessionStorage
+
+// https://blog.csdn.net/justlpf/article/details/82662365
+
+const p = new Promise((resolve, reject) => {
+  [1,2,4].map((item) => {
+    try {
+      resolve(item)
+    }catch(e) {
+      reject('123')
+    }
+  })
+})
+p.then((res) => {
+  console.log(res)
+})
