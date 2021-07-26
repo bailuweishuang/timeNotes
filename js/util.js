9b8b272c6702e57312015c42592a18f57f6609ad
huyxn <huyxn@yonyou.com>
2021-07-26 18:26:31 +0800

    biji

--- js/util.js
+++ js/util.js
@@ -6,6 +6,22 @@ const util = {
     var re = new RegExp(regu);
     return re.test(str);
   },
+  listToTree(data) {
+    const result = [];
+    const obj = {};
+    data.forEach((item) => {
+      obj[item.id] = item
+    })
+    data.forEach((item) => {
+      const parentL = obj[item.parent]
+      if (parentL) {
+        (parentL.children || (parentL.children = [])).push(item)
+      } else {
+        result.push(item)
+      }
+    })
+    return result
+  }
 };
 
 export default util;
